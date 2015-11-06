657804852e508e35004ca988e382562a665f5d29
Patte <patrick.voss1808@gmail.com>
2015-11-07 00:26:44 +0100

    Copy workflow for gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
+var del = require('del');
 
 var srcDirectory = './src';
 var srcJavascriptDirectory = srcDirectory + '/javascripts';
@@ -29,11 +30,35 @@ gulp.task('styles', function() {
               .pipe(gulp.dest(outputStylesheetsDirectory));
 });
 
-gulp.task('watch', function() {
+gulp.task('copy_html_files', function() {
+  gulp.src(srcDirectory + '/*.html')
+      .pipe(gulp.dest(outputDirectory));
+});
+
+gulp.task('copy_polymer_components', function() {
+  gulp.src(srcDirectory + '/polymer_components/**/*.html')
+      .pipe(gulp.dest(outputDirectory + '/polymer_components'));
+});
+
+gulp.task('watch_css', function() {
   return gulp.watch(srcStylesheetsDirectory, ['styles'])
             .on('change', function(event) {
               console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
             });
 });
 
-gulp.task('default', ['styles', 'watch' /*, possible other tasks... */]);
+gulp.task('watch_html', function() {
+  return gulp.watch(srcDirectory + '/*.html', ['copy_html_files'])
+            .on('change', function(event) {
+              console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+            });
+})
+
+gulp.task('watch_polymer', function() {
+  return gulp.watch(srcDirectory + '/polymer_components/**/*.html', ['copy_polymer_components'])
+            .on('change', function(event) {
+              console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+            });
+})
+
+gulp.task('default', ['styles', 'copy_html_files', 'copy_polymer_components', 'watch_css', 'watch_polymer', 'watch_html' /*, possible other tasks... */]);
